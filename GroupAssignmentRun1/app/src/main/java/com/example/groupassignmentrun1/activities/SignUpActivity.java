7ba5ab8b20164136036f296b37779b3228a4b37e
jielun2002 <jielun1105@1utar.my>
2023-08-24 01:33:20 +0800

    ui development sign in and sign out page

---
+++ GroupAssignmentRun1/app/src/main/java/com/example/groupassignmentrun1/activities/SignUpActivity.java
@@ -0,0 +1,24 @@
+package com.example.groupassignmentrun1.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.groupassignmentrun1.R;
+import com.example.groupassignmentrun1.databinding.ActivitySignUpBinding;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
+    }
+}
